36042d706fdcf05a2231fec6c777c0ad7a3c1c2c
Grinny-x <biccie71@gmail.com>
2015-07-09 19:34:33 +1000

    Update welcome.lua

--- lua/autorun/welcome.lua
+++ lua/autorun/welcome.lua
@@ -3,7 +3,7 @@ resource.AddFile("sound/ui/welcome.mp3")
 
 function WelcomeSound(ply)
         timer.Simple(5, function()
-                ply:SendLua("surface.PlaySound('ui/welcomear.mp3')")
+                ply:SendLua("surface.PlaySound('ui/welcome.mp3')")
         end)
 end
 hook.Add("PlayerInitialSpawn", "PlaySound", WelcomeSound)
